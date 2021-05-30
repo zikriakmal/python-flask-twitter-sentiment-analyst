9dba5bbac16ac2a0dbeee0d796094ad6d3febc34
zikri <zikriakmale@gmail.com>
2021-05-31 00:20:23 +0700

    selesai crude sqlite dulu

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import os
+
+file_path = os.path.abspath(os.getcwd())+"\database.db"
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Todo(db.Model):
+    id =  db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200),nullable=False)
+    completed = db.Column(db.Integer, default= 0)
+    date_created = db.Column(db.DateTime, default= datetime.utcnow )
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/', methods=['POST','GET'])
 def index():
-    return "Hello, World!"
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Something went wrong'
+
+    else :
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template("index.html",tasks=tasks) 
 
 if __name__ ==  "__main__":
     app.run(debug=True)
